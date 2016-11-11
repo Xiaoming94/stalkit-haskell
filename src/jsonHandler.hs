7922cbc498f6607d01009bcedb6cbbe3e0e58cb0
xiaoming <henyan94@gmail.com>
2016-11-11 16:45:26 +0100

    The code is now compiling

--- src/jsonHandler.hs
+++ src/jsonHandler.hs
@@ -1,23 +1,21 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
+import GHC.Generics
 import Network.HTTP.Conduit
 import qualified Data.ByteString.Lazy as B
 import Data.Aeson
 import System.IO
+import Control.Monad
 
 data Session = Session{ user_id :: String
                         ,nick :: String
-                        ,start_time :: String }
+                        ,start_time :: String
+                      } deriving (Show,Generic)
+
+instance FromJSON Session
+instance ToJSON Session
 
 getSessions :: IO B.ByteString
 getSessions = curlResults
   where
     curlResults = simpleHttp "https://hubbit.chalmers.it/sessions.json"
-
-instance FromJSON Session where
-  parseJSON (Object v) =
-    Session <$> v .: "user_id"
-            <*> v .: "nick"
-            <*> v .: "start_time"
-
-  parseJSON _ = mzero
