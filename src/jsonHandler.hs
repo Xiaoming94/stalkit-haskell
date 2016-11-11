8cb6fccfbfc94ca07e4b852bbe444c4dc7a7286b
xiaoming <henyan94@gmail.com>
2016-11-11 22:44:07 +0100

    Added some code that is commented to understand where to start working with the IO Monad

--- src/jsonHandler.hs
+++ src/jsonHandler.hs
@@ -19,3 +19,21 @@ getSessions :: IO (Maybe [Session])
 getSessions = decode <$> curlResults
   where
     curlResults = simpleHttp "https://hubbit.chalmers.it/sessions.json"
+
+{-compDiff :: IO (Maybe [Session])
+compDiff = do
+  sess1 <- getSessions
+  sleep 20000
+  sess2 <- if isJust sess1 then getSessions else Nothing
+  return $ do
+    s1 <- sess1
+    s2 <- sess2
+    return $ diff s1 s2
+
+u = undefined
+
+diff :: [Session] -> [Session] -> [Session]
+diff [] _ = []
+diff xs [] = []
+diff x:xs
+-}
