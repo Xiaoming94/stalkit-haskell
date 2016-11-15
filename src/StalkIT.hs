24ca2f10b0415444717af2586224a87d30fc3125
xiaoming <henyan94@gmail.com>
2016-11-15 12:18:59 +0100

    Started making the handlesession function

--- src/StalkIT.hs
+++ src/StalkIT.hs
@@ -1,14 +1,20 @@
 import Control.Monad
 import Data.Maybe
 import JsonHandler
+import Control.Concurrent
+
+sleepingTime = 10000000
 
 main :: IO()
 main = stalkIT getSessions
 
 stalkIT :: IO (Maybe [Session]) -> IO ()
 stalkIT sess1 = do
+  threadDelay sleepingTime
   let sess2 = getSessions
   s1 <- sess1
   s2 <- sess2
-  print s1
-  print s2
+  handleSessions fromJust(s1) fromJust(s2)
+
+handleSession :: [Session] -> [Session] -> [Session]
+handleSession s1 s2 = s1
