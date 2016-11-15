828818b5113e4c67dc85b43f3dce09408bb90c2b
xiaoming <henyan94@gmail.com>
2016-11-15 12:01:22 +0100

    Started the development of stalkit function

--- src/StalkIT.hs
+++ src/StalkIT.hs
@@ -2,7 +2,13 @@ import Control.Monad
 import Data.Maybe
 import JsonHandler
 
-main = do
-  sessions <- getSessions
-  print "Hello World"
-  print sessions
+main :: IO()
+main = stalkIT getSessions
+
+stalkIT :: IO (Maybe [Session]) -> IO ()
+stalkIT sess1 = do
+  let sess2 = getSessions
+  s1 <- sess1
+  s2 <- sess2
+  print s1
+  print s2
